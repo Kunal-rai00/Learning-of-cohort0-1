dec367988d994c62f2233f82b615fb1cc37ecfdb
Kunal-rai00 <krai52961@gmail.com>
2024-08-08 23:50:37 +0530

    solved javascript hard todo asignment

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -11,7 +11,40 @@
 */
 
 class Todo {
-
+  constructor() {
+    this.todos = [];
+  }
+  add(todo) {
+    this.todos.push(todo);
+  }
+  remove(indexOfTodo) {
+    if (indexOfTodo >= 0 && indexOfTodo < this.todos.length) {
+      this.todos.splice(indexOfTodo, 1);
+    }
+  }
+  update(index, updatedTodo) {
+    if (index >= 0 && index < this.todos.length) {
+      this.todos[index] = updatedTodo;
+    } else {
+      console.error("Index out of bounds");
+    }
+  }
+  getAll() {
+    return this.todos;
+  }
+  get(indexOfTodo) {
+    if (indexOfTodo >= 0 && indexOfTodo < this.todos.length) {
+      return this.todos[indexOfTodo];
+    }
+    return null;
+  }
+  clear() {
+    this.todos = [];
+  }
 }
 
+let todo = new Todo();
+todo.add("kuallll");
+
+console.log(todo.getAll());
 module.exports = Todo;
